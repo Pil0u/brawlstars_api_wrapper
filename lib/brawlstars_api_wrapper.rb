5bde1e3fd73413a1341c03e14c75b409480c98a9
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2020-09-21 19:58:12 +0200

    Added HTTParty & dotenv gems

--- lib/brawlstars_api_wrapper.rb
+++ lib/brawlstars_api_wrapper.rb
@@ -1,4 +1,7 @@
+# require "dotenv/load" # Uncomment this when in dev to use the dotenv gem
+require "httparty"
 require "brawlstars_api_wrapper/version"
+require "brawlstars_api_wrapper/client"
 
 module BrawlstarsApiWrapper
   class Error < StandardError; end
